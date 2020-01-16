ed91a2c9fc7e710deeffe577147e68e4aa598b45
rinaldo <rinaldobenaccetta@hotmail.com>
2020-01-16 17:21:42 +0100

    fix tab

--- src/test/is_undefined.test.js
+++ src/test/is_undefined.test.js
@@ -2,7 +2,7 @@ const esmImport = require("esm")(module);
 const mod = esmImport("../tools/is_undefined.js");
 
 test("let x = 1 to be defined", () => {
-    let x = 1;
+  let x = 1;
   expect(mod.$isUndefined(x)).toBe(false);
 });
 
