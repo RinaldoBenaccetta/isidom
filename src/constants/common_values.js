3f623b39ba140d7cabd8c49fd0b3d3358ceac9c9
rinaldo <rinaldobenaccetta@hotmail.com>
2020-01-16 11:26:05 +0100

    implement constants

---
+++ src/constants/common_values.js
@@ -0,0 +1,55 @@
+/**
+ * 
+ * List of common values.
+ * $$ say non-string value.
+ * $$_ say string value.
+ */
+
+export const $$true = true;
+export const $$false = false;
+export const $$null = null;
+export const $$_true = "true";
+export const $$_false = "false";
+export const $$_null = "null";
+
+
+/**
+ * Deprecated values.
+ */
+export const $$undefined = "undefined";
+export const $$click = "click";
+export const $$mouseup = "mouseup";
+export const $$default = "default";
+export const $$change = "change";
+export const $$blur = "blur";
+export const $$focus = "focus";
+
+
+/**
+ * Deprecated constants should be replaced by theses.
+ */
+export const $$_undefined = "undefined";
+export const $$_click = "click";
+export const $$_mouseup = "mouseup";
+export const $$_default = "default";
+export const $$_change = "change";
+export const $$_blur = "blur";
+export const $$_focus = "focus";
+
+
+export const $$_body = "body";
+export const $$_html = "html";
+export const $$_img = "img";
+export const $$_px = "px";
+export const $$_em = "em";
+export const $$_rem = "rem";
+export const $$_emptyString = "";
+export const $$_none = "none";
+export const $$_initial = "initial";
+export const $$_hidden = "hidden";
+export const $$_ms = "ms";
+export const $$_block = "block";
+export const $$_ok = "ok";
+export const $$_error = "error";
+export const $$_no = "no";
+export const $$_disabled = "disabled";
