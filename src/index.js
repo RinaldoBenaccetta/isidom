86ad45ce40a7be48610053dc768553d6e89f5442
rinaldo <rinaldobenaccetta@hotmail.com>
2020-01-16 14:56:48 +0100

    add - index js

---
+++ src/index.js
@@ -0,0 +1,23 @@
+
+
+import {
+  $$true,
+  $$false,
+  $$null,
+  $$_true,
+  $$_false,
+  $$_null,
+  $$undefined
+} from "./constants/common_values.js";
+
+import $isUndefined from "./tools/is_undefined.js";
+
+console.log("hello " + $$undefined);
+
+let x = 2;
+if (!$isUndefined(x)) {
+  console.log('defined');
+} else {
+  console.log('undefined');
+  
+}
