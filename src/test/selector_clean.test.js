7609be761d3967caad89420301866c9717ffe069
rinaldo <rinaldobenaccetta@hotmail.com>
2020-01-30 16:32:13 +0100

    add - add trim spaces at end and start in selector_clean function

--- src/test/selector_clean.test.js
+++ src/test/selector_clean.test.js
@@ -7,14 +7,29 @@ describe('selector_clean', () => {
             expect(mod.$selectorClean(className)).toBe("myClass");
         });
 
+        test("wich is a class name with spaces at start and end return the class name without . and spaces at start and end of the string", () => {
+          let className = "   .myClass  ";
+          expect(mod.$selectorClean(className)).toBe("myClass");
+        });
+
         test("wich is a id name return the id name without #", () => {
           let idName = "#myId";
+          expect(mod.$selectorClean(idName)).toBe("myId");
+        });
+
+        test("wich is a id name with spaces at start and end return the id name without # and spaces at start and end of the string", () => {
+          let idName = "   #myId  ";
           expect(mod.$selectorClean(idName)).toBe("myId");
         });
 
         test("wich is a string return the string", () => {
           let string = "myString";
           expect(mod.$selectorClean(string)).toBe(string);
+        });
+
+        test("wich is a string with spaces at start and end return the string without spaces at start and end of the string", () => {
+          let string = "    myString  ";
+          expect(mod.$selectorClean(string)).toBe("myString");
         });
     });
 });
--- src/tools/selector_clean.js
+++ src/tools/selector_clean.js
@@ -1,10 +1,12 @@
 /**
  * @description Remove '.' and '#' from start of selectors names.
  *              '.myClass' become 'myClass' and '#myId' become 'myId'
+ *              The spaces at the start and end of the string will be removed.
  * @param {string} selector
  * @returns 
  */
 export function $selectorClean(selector) {
+  selector = selector.trim();
   if (selector.charAt(0) === "#" || selector.charAt(0) === ".") {
     selector = selector.substr(1);
   }
