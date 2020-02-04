7ca3fb2efcb69e9ff5347c97d4749feae6ea4c97
rinaldo <rinaldobenaccetta@hotmail.com>
2020-02-04 12:03:46 +0100

    up: add constants and functions to index

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-export * from './tools/is-undefined'
+export * from './constants/elements'
 export * from './constants/values'
+
+export * from './tools/array-to-string-list'
+export * from './tools/is-array'
+export * from './tools/is-nodelist'
+export * from './tools/is-undefined'
+export * from './tools/selector-clean'
