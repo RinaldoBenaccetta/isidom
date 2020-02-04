53fc5632fcaaa3c30f93041179c936986c3139b8
rinaldo <rinaldobenaccetta@hotmail.com>
2020-02-04 12:04:59 +0100

    lint: clean rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 export default {
-  input: "src/index.js",
+  input: 'src/index.js',
   output: {
-    file: "dist/bundle.js",
-    format: "es"
+    file: 'dist/bundle.js',
+    format: 'es'
   }
-};
+}
