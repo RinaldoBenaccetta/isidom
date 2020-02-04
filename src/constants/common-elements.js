7859604afbcd0b18f5bf461a5e2b9c18500f9310
rinaldo <rinaldobenaccetta@hotmail.com>
2020-02-04 11:11:05 +0100

    refactor: change naming convention for variables

--- src/constants/common-elements.js
+++ src/constants/common-elements.js
@@ -6,7 +6,7 @@
  * Exemple : $$window.content is the same as window.content but will be minified like this : a.content.
  */
 
-export const $$window = window
-export const $$document = document
-export const $$docElt = $$document.documentElement
-export const $$docBody = $$document.body
+export const $window = window
+export const $document = document
+export const $docElement = $document.documentElement
+export const $docBody = $document.body
