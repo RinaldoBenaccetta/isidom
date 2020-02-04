07f600459e380bd928edd2c49ccaeed3da33b377
rinaldo <rinaldobenaccetta@hotmail.com>
2020-02-04 16:28:16 +0100

    improve: add test for triming to selector array clean test

--- src/test/selector-array-clean.test.js
+++ src/test/selector-array-clean.test.js
@@ -3,11 +3,12 @@ import * as mod from '../tools/selector-array-clean'
 describe('$selectorArrayClean', () => {
   describe('given an array of selectors', () => {
     test('wich is a mix of class, id and string return an array of string without . or # at the start and without space at start and end of the string', () => {
-      const SELECTOR_ARRAY = ['myClass', '.myOtherClass', '#myId']
+      const SELECTOR_ARRAY = ['myClass', '.myOtherClass', '#myId', '  anOtherSelector   ']
       expect(mod.$selectorArrayClean(SELECTOR_ARRAY)).toStrictEqual([
         'myClass',
         'myOtherClass',
-        'myId'
+        'myId',
+        'anOtherSelector'
       ])
     })
     test('wich is a string return an array of the string without . or # at the start and without space at start and end of the string', () => {
