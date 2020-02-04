911b019f847f1a838ab45fa682298a2e367b3073
rinaldo <rinaldobenaccetta@hotmail.com>
2020-02-04 10:52:48 +0100

    lint: clean docblocks

--- src/tools/is-nodelist.js
+++ src/tools/is-nodelist.js
@@ -1,7 +1,10 @@
 /**
  * @description Check if input is a nodelist and return true or false.
+ *
  * @export
+ *
  * @param {*} input
+ *
  * @returns {boolean}
  */
 export function $isNodeList (input) {
--- src/tools/is-undefined.js
+++ src/tools/is-undefined.js
@@ -3,8 +3,11 @@ import * as constant from '../constants/common-values'
 /**
  * @description Replacement for typeof x === 'undefined'
  *              To be used like this : if(isUndefined(x)) then ...
+ *
  * @export
+ *
  * @param {*} variable
+ *
  * @returns {boolean}
  */
 export function $isUndefined (variable) {
