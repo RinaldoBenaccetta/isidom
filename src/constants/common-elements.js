6ae6bbefe573587d1e18c7cca3a1d3546abb7035
rinaldo <rinaldobenaccetta@hotmail.com>
2020-02-04 10:49:55 +0100

    lint: clean common elements

--- src/constants/common-elements.js
+++ src/constants/common-elements.js
@@ -1,12 +1,12 @@
 /**
- * 
+ *
  * List of common used elements.
  * $$ say non-string.
  * These values are useable for maximum minification.
  * Exemple : $$window.content is the same as window.content but will be minified like this : a.content.
  */
 
-export const $$window = window;
-export const $$document = document;
-export const $$docElt = $$document.documentElement;
-export const $$docBody = $$document.body;
+export const $$window = window
+export const $$document = document
+export const $$docElt = $$document.documentElement
+export const $$docBody = $$document.body
