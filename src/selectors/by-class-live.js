845d90abb81dd17eefc70c2e69f8a0d2edd78256
rinaldo <rinaldobenaccetta@hotmail.com>
2020-02-06 10:30:09 +0100

    doc: modify docblock of by class live

--- src/selectors/by-class-live.js
+++ src/selectors/by-class-live.js
@@ -5,14 +5,14 @@ import * as elements from '../constants/elements'
 /**
  * @description Select elements based on getElementsByClassName wich return an HTMLCollection of elements containing specified class.
  *              An HTMLCollection is live : a modification in the DOM is reflected in the HTMLCollection.
- *              An HTMLCollection nodeList don't accept forEach. Use other loops with it.
+ *              An HTMLCollection don't accept forEach. Use other loops with it.
  *
  * @export
  *
  * @param {string, string[]} className The string and strings in array can start with a dot or not.
  *                                     The string and strings in array can have spaces at start an end.
  * @param {HTMLElement=} [element=elements.$document]
- * 
+ *
  * @returns
  */
 export function $byClassLive (className, element = elements.$document) {
