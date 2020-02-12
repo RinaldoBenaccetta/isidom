f69c46612c4ce1b8a9b355db9c43a6cb7bf9d339
rinaldo <rinaldobenaccetta@hotmail.com>
2020-02-12 11:01:57 +0100

    improve: add is collection support to the for each function

--- src/tools/for-each.js
+++ src/tools/for-each.js
@@ -1,5 +1,6 @@
 import { $isNodeList } from './is-nodelist'
 import { $isArray } from './is-array'
+import { $isCollection } from './is-collection'
 
 /**
  * @description Replacement for the forEach native function from Javascript based on for loop.
@@ -8,18 +9,14 @@ import { $isArray } from './is-array'
  *
  * @param {function} functionToExecute This is the function that will be executed in the forEach loop.
  *
- * @param {*} element This is the array or the nodelist that will be processed by the forEach loop.
+ * @param {*} element This is the nodelist, the nodeList or the HTMLCollection that will be processed by the for loop.
  *                    If this is not an array or a nodelist, the function will be executed once on the argument.
  */
 export function $forEach (functionToExecute, element) {
-  if ($isNodeList(element) || $isArray(element)) {
+  if ($isNodeList(element) || $isArray(element) || $isCollection(element)) {
     for (const item of element) {
       functionToExecute(item)
     }
-
-    // element.forEach(function (item) {
-    //   functionToExecute(item)
-    // })
   } else {
     functionToExecute(element)
   }
