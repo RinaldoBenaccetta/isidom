200399cfd755a434775b7c294efd5d88d5bc96a7
rinaldo <rinaldobenaccetta@hotmail.com>
2020-02-12 14:43:31 +0100

    doc: improve removeClass docblock

--- src/manipulators/classes/remove-class.js
+++ src/manipulators/classes/remove-class.js
@@ -6,6 +6,12 @@ import * as elements from '../../constants/elements'
  * @description Remove class or a list of classes from an HTMLElement or
  *              from all HTMLElement of a nodeList or an HTMLCollection.
  *
+ *              If in application, classes are removed only on HTMLELement a few
+ *              number of times, the best for minification would be to use this
+ *              instead of $removeClass : myElement[$classList].remove(class)
+ *              That's not apply if $forEach or $addClass is allready used,
+ *              in this case, the best is to use $removeClass.
+ *
  * @export
  *
  * @param {sting, string[]} className
