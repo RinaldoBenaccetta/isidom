0473c2eb64f1da8bab860bcd15efbf449d96e2d9
rinaldo <rinaldobenaccetta@hotmail.com>
2020-02-13 14:38:31 +0100

    feature: get attributes function

---
+++ src/manipulators/attributes/get-attributes.js
@@ -0,0 +1,24 @@
+/**
+ * @description Return an object filled with all attributes and values of
+ *              a specified HTMLElement.
+ *              The returned object is like this :
+ *                {
+ *                    id: 'myId'
+ *                    class: 'myClass'
+ *                    type: 'range'
+ *                }
+ *
+ * @export
+ *
+ * @param {HTMLElement} element
+ *
+ * @returns {Object}
+ */
+export function $getAttributes (element) {
+  const OUTPUT = {}
+  for (let i = 0, attributes = element.attributes; i < attributes.length; i++) {
+    OUTPUT[attributes[i].nodeName] = attributes[i].nodeValue
+  }
+
+  return OUTPUT
+}
---
+++ src/test/manipulators/attributes/get-attributes.test.js
@@ -0,0 +1,32 @@
+import { $getAttributes } from '../../../manipulators/attributes/get-attributes'
+import '@testing-library/jest-dom'
+
+const DOC =
+  // eslint-disable-next-line no-multi-str
+  '<div class="myFirstClass myFirdClass">hello</div>\
+  <div class="mySecondClass"></div>\
+  <div class="myFirdClass">world</div>\
+  <div class="myFourthClass"></div>\
+  <div class ="myFirstClass">!</div>\
+  <div class ="myFifthClass mySixthClass">!</div>\
+  <div class="myFirstClass">\
+    <div class="myFirstClass"></div>\
+    <div class="myFirdClass mySecondClass"></div>\
+    <input type="range" min="10" max="125" id="myId"></input>\
+    <div class="mySecondClass myThirdClass"></div>\
+  </div>'
+
+describe('$getAttributes', () => {
+  describe('given an HTMLElement', () => {
+    test('wich is a valid HTMLElement return an object with all the attributes of the HTMLElement', () => {
+      document.body.innerHTML = DOC
+      const TARGET_ID = document.getElementById('myId')
+      const ATTRIBUTES = $getAttributes(TARGET_ID)
+      expect(ATTRIBUTES.min).toBe('10')
+      expect(ATTRIBUTES.id).toBe('myId')
+      expect(ATTRIBUTES.max).toBe('125')
+      expect(ATTRIBUTES.type).toBe('range')
+      expect(ATTRIBUTES instanceof Object).toBeTruthy()
+    })
+  })
+})
