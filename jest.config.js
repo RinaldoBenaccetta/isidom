ee496a2425ee7d1d8034d2d1fe231a2109763ea5
rinaldo <rinaldobenaccetta@hotmail.com>
2020-05-06 15:14:47 +0200

    test: add coverage

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,15 @@ module.exports = {
   verbose: true,
   preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['./jest.setup.js']
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '!**/*.experiment.js',
+    '**/*.js',
+    '!**/test/fixtures/**/*.js',
+    '!**/test/coverage/**/*.js',
+    '!/node_modules/',
+    '!**/*.config.js'
+  ],
+  coverageDirectory: 'src/test/coverage'
 }
